5753416202c49ee570cb11a591af28ef91b6ee0a
Sheenaz <taraschorny10@gmail.com>
2024-05-16 12:49:35 +0300

    Update foo.py

--- foo.py
+++ foo.py
@@ -1,1 +1,4 @@
-https://docs.google.com/presentation/d/1A6fj1LZp4XIBon099yTDdXPXw2Q6Bdkyl3Us2efUXG4/edit?usp=drivesdk
+[ФІЗИКА](https://docs.google.com/presentation/d/1A6fj1LZp4XIBon099yTDdXPXw2Q6Bdkyl3Us2efUXG4/edit?usp=drivesdk)
+
+[ЛІТЕРАТУРА](https://docs.google.com/presentation/d/1cltM1tHZqpdkLtb_K0O1r-udTvx6Bd83n9-VFRhdU2w/edit?usp=drivesdk)
+
